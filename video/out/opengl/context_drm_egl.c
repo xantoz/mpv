03d76f497b3e76883fd217f427f4459de62a4190
Anton Kindestam <antonki@kth.se>
2018-05-04 12:05:03 +0200

    drm_common: Add proper help option to drm-mode
    This was implemented by using OPT_STRING_VALIDATE for drm-mode,
    instead of OPT_INT. Using a string here also prepares for future
    additions to drm-mode that aim to allow specifying a mode by its
    resolution.

--- video/out/opengl/context_drm_egl.c
+++ video/out/opengl/context_drm_egl.c
@@ -590,7 +590,7 @@ static bool drm_egl_init(struct ra_ctx *ctx)
 
     MP_VERBOSE(ctx, "Initializing KMS\n");
     p->kms = kms_create(ctx->log, ctx->vo->opts->drm_opts->drm_connector_spec,
-                        ctx->vo->opts->drm_opts->drm_mode_id,
+                        ctx->vo->opts->drm_opts->drm_mode_spec,
                         ctx->vo->opts->drm_opts->drm_osd_plane_id,
                         ctx->vo->opts->drm_opts->drm_video_plane_id);
     if (!p->kms) {
