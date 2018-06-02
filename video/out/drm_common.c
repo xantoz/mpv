55731c9c4bfa8a339d85c8d605c506dbedbf7dfa
Anton Kindestam <antonki@kth.se>
2018-06-02 12:54:01 +0200

    drm_common: Don't export functions only being used internally
    As far as I know none of these functions were being used outside of
    drm_common, nor should there really be a need to use them.

--- video/out/drm_common.c
+++ video/out/drm_common.c
@@ -43,6 +43,11 @@
 
 static int vt_switcher_pipe[2];
 
+static int drm_validate_connector_opt(struct mp_log *log, const struct m_option *opt, struct bstr name, struct bstr param);
+static int drm_validate_mode_opt(struct mp_log *log, const struct m_option *opt, struct bstr name, struct bstr param);
+static void kms_show_available_modes(struct mp_log *log, const drmModeConnector *connector);
+static void kms_show_available_connectors(struct mp_log *log, int card_no);
+
 #define OPT_BASE_STRUCT struct drm_opts
 const struct m_sub_options drm_conf = {
     .opts = (const struct m_option[]) {
@@ -378,7 +383,7 @@ static double mode_get_Hz(const drmModeModeInfo *mode)
     return mode->clock * 1000.0 / mode->htotal / mode->vtotal;
 }
 
-void kms_show_available_modes(
+static void kms_show_available_modes(
     struct mp_log *log, const drmModeConnector *connector)
 {
     for (unsigned int i = 0; i < connector->count_modes; i++) {
@@ -431,7 +436,7 @@ static void kms_show_connector_name_and_state_callback(struct mp_log *log, int c
             (connector->connection == DRM_MODE_CONNECTED) ? "connected" : "disconnected");
 }
 
-void kms_show_available_connectors(struct mp_log *log, int card_no)
+static void kms_show_available_connectors(struct mp_log *log, int card_no)
 {
     mp_info(log, "Available connectors for card %d:\n", card_no);
     kms_show_foreach_connector(log, card_no, kms_show_connector_name_and_state_callback);
@@ -451,7 +456,7 @@ static void kms_show_connector_modes_callback(struct mp_log *log, int card_no,
     mp_info(log, "\n");
 }
 
-void kms_show_available_connectors_and_modes(struct mp_log *log, int card_no)
+static void kms_show_available_connectors_and_modes(struct mp_log *log, int card_no)
 {
     kms_show_foreach_connector(log, card_no, kms_show_connector_modes_callback);
 }
@@ -467,12 +472,12 @@ static void kms_show_foreach_card(struct mp_log *log, void (*show_fn)(struct mp_
     }
 }
 
-void kms_show_available_cards_and_connectors(struct mp_log *log)
+static void kms_show_available_cards_and_connectors(struct mp_log *log)
 {
     kms_show_foreach_card(log, kms_show_available_connectors);
 }
 
-void kms_show_available_cards_connectors_and_modes(struct mp_log *log)
+static void kms_show_available_cards_connectors_and_modes(struct mp_log *log)
 {
     kms_show_foreach_card(log, kms_show_available_connectors_and_modes);
 }
@@ -482,8 +487,8 @@ double kms_get_display_fps(const struct kms *kms)
     return mode_get_Hz(&kms->mode.mode);
 }
 
-int drm_validate_connector_opt(struct mp_log *log, const struct m_option *opt,
-                               struct bstr name, struct bstr param)
+static int drm_validate_connector_opt(struct mp_log *log, const struct m_option *opt,
+                                      struct bstr name, struct bstr param)
 {
     if (bstr_equals0(param, "help")) {
         kms_show_available_cards_and_connectors(log);
@@ -492,8 +497,8 @@ int drm_validate_connector_opt(struct mp_log *log, const struct m_option *opt,
     return 1;
 }
 
-int drm_validate_mode_opt(struct mp_log *log, const struct m_option *opt,
-                          struct bstr name, struct bstr param)
+static int drm_validate_mode_opt(struct mp_log *log, const struct m_option *opt,
+                                 struct bstr name, struct bstr param)
 {
     if (bstr_equals0(param, "help")) {
         kms_show_available_cards_connectors_and_modes(log);
